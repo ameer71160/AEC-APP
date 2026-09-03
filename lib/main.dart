d8a91c7fd248c9fefcf748af475ed0367b8ae34a
ameer71160 <308467617+ameer71160@users.noreply.github.com>
2026-09-03 20:15:00 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/dashboard_screen.dart';
+import 'screens/admin_login_screen.dart';
+import 'screens/admin_dashboard.dart';
+import 'screens/members_list_screen.dart';
+import 'screens/member_detail_screen.dart';
+import 'screens/deposit_screen.dart';
+import 'screens/features_screen.dart';
+import 'screens/terms_screen.dart';
+import 'screens/privacy_screen.dart';
+import 'screens/my_notifications_screen.dart';
+import 'screens/about_screen.dart';
+import 'screens/archive_screen.dart';
+import 'utils/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AEC Community',
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/dashboard': (context) => const DashboardScreen(),
+        '/admin_login': (context) => const AdminLoginScreen(),
+        '/admin_dashboard': (context) => const AdminDashboard(),
+        '/members_list': (context) => const MembersListScreen(),
+        '/member_detail': (context) => const MemberDetailScreen(),
+        '/deposit': (context) => const DepositScreen(),
+        '/features': (context) => const FeaturesScreen(),
+        '/terms': (context) => const TermsScreen(),
+        '/privacy': (context) => const PrivacyScreen(),
+        '/notifications': (context) => const MyNotificationsScreen(),
+        '/about': (context) => const AboutScreen(),
+        '/archive': (context) => const ArchiveScreen(),
+      },
+    );
+  }
+}
