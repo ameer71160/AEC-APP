ea776760644d648677860e7ae0d22f9c52e105ac
ameer71160 <308467617+ameer71160@users.noreply.github.com>
2026-09-03 20:31:45 +0300

    Create animated_text.dart

---
+++ lib/widgets/animated_text.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AnimatedText extends StatefulWidget {
+  final String text;
+  final double speed;
+  final TextStyle style;
+  final TextAlign textAlign;
+
+  const AnimatedText({
+    super.key,
+    required this.text,
+    this.speed = 50,
+    this.style = const TextStyle(fontSize: 16),
+    this.textAlign = TextAlign.center,
+  });
+
+  @override
+  State<AnimatedText> createState() => _AnimatedTextState();
+}
+
+class _AnimatedTextState extends State<AnimatedText> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: widget.text.length * widget.speed ~/ 2),
+      vsync: this,
+    );
+    _animation = Tween<Offset>(begin: Offset(-1, 0), end: Offset(1, 0)).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.linear),
+    );
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animation,
+      child: Text(
+        widget.text,
+        style: widget.style,
+        textAlign: widget.textAlign,
+      ),
+    );
+  }
+}
